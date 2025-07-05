c6d5f6e559fa950fab1ae6e63af464bf59f4e548
pon <mamepon@gmail.com>
2025-07-05 20:27:35 +0900

    next.config.jsにwebpackの設定を追加し、エイリアスを'src'ディレクトリに設定しました。これにより、インポートパスの管理が容易になり、コードの可読性が向上します。

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,6 +9,13 @@ const nextConfig = {
         hostname: '*.microcms-assets.io',
       },
     ],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
   },
 }
 
